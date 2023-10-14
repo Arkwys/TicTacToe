9deb5224ba3fce22acc043e961a799b4b4791ed9
Arkwys <sabalex@hotmail.ca>
2023-10-14 16:09:47 -0400

    First CPUPlayer try

--- src/CPUPlayer.java
+++ src/CPUPlayer.java
@@ -1,45 +1,120 @@
 import java.util.ArrayList;
 
-// IMPORTANT: Il ne faut pas changer la signature des méthodes
-// de cette classe, ni le nom de la classe.
-// Vous pouvez par contre ajouter d'autres méthodes (ça devrait 
-// être le cas)
 class CPUPlayer {
     private int numExploredNodes;
-    private Mark cpuMark; // Le joueur MAX (X ou O)
+    private Mark cpu;
 
-    // Le constructeur reçoit en paramètre le
-    // joueur MAX (X ou O)
     public CPUPlayer(Mark cpu) {
-        this.cpuMark = cpu;
+        this.cpu = cpu;
     }
 
-    // Ne pas changer cette méthode
     public int getNumOfExploredNodes() {
         return numExploredNodes;
     }
 
-    // Retourne la liste des coups possibles en utilisant l'algorithme Minimax
     public ArrayList<Move> getNextMoveMinMax(Board board) {
         numExploredNodes = 0;
-
         ArrayList<Move> possibleMoves = new ArrayList<>();
+        int bestScore = Integer.MIN_VALUE;
 
-        // À implémenter : Utiliser l'algorithme Minimax pour générer les coups possibles
-        // Ajouter les coups possibles à la liste possibleMoves
+        for (Move move : board.getAvailableMoves()) {
+            board.play(move, cpu);
+            int score = minimax(board, 0, false);
+            board.play(move, Mark.EMPTY);
+
+            if (score > bestScore) {
+                possibleMoves.clear();
+                bestScore = score;
+            }
+
+            if (score == bestScore) {
+                possibleMoves.add(move);
+            }
+        }
 
         return possibleMoves;
     }
 
-    // Retourne la liste des coups possibles en utilisant l'algorithme Alpha-Beta Pruning
     public ArrayList<Move> getNextMoveAB(Board board) {
         numExploredNodes = 0;
-
         ArrayList<Move> possibleMoves = new ArrayList<>();
+        int bestScore = Integer.MIN_VALUE;
+        int alpha = Integer.MIN_VALUE;
+        int beta = Integer.MAX_VALUE;
 
-        // À implémenter : Utiliser l'algorithme Alpha-Beta Pruning pour générer les coups possibles
-        // Ajouter les coups possibles à la liste possibleMoves
+        for (Move move : board.getAvailableMoves()) {
+            board.play(move, cpu);
+            int score = alphaBeta(board, 0, alpha, beta, false);
+            board.play(move, Mark.EMPTY);
+
+            if (score > bestScore) {
+                possibleMoves.clear();
+                bestScore = score;
+            }
+
+            if (score == bestScore) {
+                possibleMoves.add(move);
+            }
+
+            alpha = Math.max(alpha, bestScore);
+
+            if (alpha >= beta) {
+                return possibleMoves; // Prune the remaining branches
+            }
+        }
 
         return possibleMoves;
+    }
+
+    private int minimax(Board board, int depth, boolean isMaximizing) {
+        numExploredNodes++;
+
+        int result = board.evaluate(cpu);
+        if (result != 0) {
+            return result;
+        }
+
+        int bestScore = isMaximizing ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+
+        for (Move move : board.getAvailableMoves()) {
+            board.play(move, isMaximizing ? cpu : board.getOpponentMark(cpu));
+            int score = minimax(board, depth + 1, !isMaximizing);
+            board.play(move, Mark.EMPTY);
+
+            bestScore = isMaximizing ? Math.max(score, bestScore) : Math.min(score, bestScore);
+        }
+
+        return bestScore;
+    }
+
+    private int alphaBeta(Board board, int depth, int alpha, int beta, boolean isMaximizing) {
+        numExploredNodes++;
+
+        int result = board.evaluate(cpu);
+        if (result != 0) {
+            return result;
+        }
+
+        int bestScore = isMaximizing ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+
+        for (Move move : board.getAvailableMoves()) {
+            board.play(move, isMaximizing ? cpu : board.getOpponentMark(cpu));
+            int score = alphaBeta(board, depth + 1, alpha, beta, !isMaximizing);
+            board.play(move, Mark.EMPTY);
+
+            if (isMaximizing) {
+                bestScore = Math.max(score, bestScore);
+                alpha = Math.max(alpha, bestScore);
+            } else {
+                bestScore = Math.min(score, bestScore);
+                beta = Math.min(beta, bestScore);
+            }
+
+            if (alpha >= beta) {
+                return bestScore; // Prune the remaining branches
+            }
+        }
+
+        return bestScore;
     }
 }
